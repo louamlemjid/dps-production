7bfeed885cdad748e691f55f1d340f5fac348dfe
louamlemjid <elmejidlouem@gmail.com>
2024-12-05 09:30:03 +0100

    changed the TOKEN to Bearer Password123 to work with authorization header

--- src/routes/authenticationRoute.ts
+++ src/routes/authenticationRoute.ts
@@ -1,8 +1,8 @@
 import { Request,Response,NextFunction } from "express";
 
 export const authenticate = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers['authorization']; 
-    if (token === process.env.AUTH_TOKEN) {
+    const token = req.headers['authorization'];
+    if (token == process.env.AUTH_TOKEN) {
         next();
     } else {
         res.status(403).json({ error: "Forbidden: Invalid token" });
